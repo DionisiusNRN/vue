c234be0656dcc7ff52519dd98f9cab8454bc5c41
Dion <149992425+DionisiusNRN@users.noreply.github.com>
2024-06-06 19:18:11 +0700

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="container mt-5">
+    <h1>IDShop</h1>
+    <p class="animated fadeInRight">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam dolore
+      dolorem explicabo praesentium et aperiam provident. Aspernatur non dolorum
+      iste quos odit at corporis totam adipisci. Aspernatur quam tenetur labore.
+    </p>
+    <font-awesome-icon icon="shopping-cart"></font-awesome-icon>
+  </div>
+</template>
+
+<script>
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+export default {
+  name: "App",
+  components: {
+    FontAwesomeIcon,
+  },
+};
+</script>
